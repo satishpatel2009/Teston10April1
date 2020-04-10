c5ec03b173a70a16f1e541d8e6c763f4b4de42f5
satish <62432137+satishpatel2009@users.noreply.github.com>
2020-04-10 19:01:42 -0400

    Add project files.

---
+++ 1Consol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1Consol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
